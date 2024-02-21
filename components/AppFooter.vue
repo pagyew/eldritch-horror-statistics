181dc91609070a71c67f6406760e2cbea4074876
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-02-21 11:50:41 +0600

    feat: create AppFooter.vue and AppHeader.vue

---
+++ components/AppFooter.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <footer>
+    It's footer
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  border: 1px solid black;
+  padding: 10px;
+}
+</style>
---
+++ components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header>
+    It's header
+    <nav>
+      <ul>
+        <li>
+          <NuxtLink to="/">Home page</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/games">My Games</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/expansions">My Expansions</NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/statistics">My Statistics</NuxtLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  border: 1px solid black;
+  padding: 10px;
+}
+</style>
