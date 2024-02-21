53af73c629dcc721e387971b02fdeb37081bde30
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-02-21 11:42:07 +0600

    build(deps): update nuxt.config.ts, package-lock.json and package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: {
+    enabled: false,
+  },
+
+  app: {
+    rootId: 'app'
+  },
+
+  routeRules: {
+    '/api/**': { cors: true }
+  },
+
+  experimental: {
+    appManifest: false
+  },
+
+  modules: ['@pinia/nuxt', "@vueuse/nuxt"]
 })
