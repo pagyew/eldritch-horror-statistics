fe463377a3a4d51987d27d75ea23113a5db728fb
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-02-27 23:27:51 +0600

    update config: adds auto-imports

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,24 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: {
-    enabled: false
-  },
+  devtools: { enabled: false },
 
-  typescript: {
-    shim: false
-  },
+  typescript: { shim: false },
 
-  app: {
-    rootId: 'app'
-  },
+  app: { rootId: 'app' },
 
   routeRules: {
     '/': { prerender: true },
     '/api/*': { cors: true }
   },
 
-  experimental: {
-    appManifest: false
+  experimental: { appManifest: false },
+
+  imports: {
+    dirs: ['constants']
+  },
+
+  nitro: {
+    imports: {
+      dirs: ['constants', 'server/mocks']
+    }
   },
 
   modules: ['@pinia/nuxt', "@vueform/nuxt"]
