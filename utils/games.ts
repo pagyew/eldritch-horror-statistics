df3ea30697ab2f29cbbc42ea5b54c7a5a0ad12d3
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-02-29 16:17:37 +0600

    refactor: utils

--- utils/games.ts
+++ utils/games.ts
@@ -3,5 +3,5 @@ export function isWinner(game: IGame): game is IGameWinning {
 }
 
 export function calculateScore(game: IGameWinning) {
-  return Object.values(game.result.scoring).reduce((a, b) => a + b)
+  return Object.values(game.result.scoring).reduce((a, b) => a + b, 0)
 }
