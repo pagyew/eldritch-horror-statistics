c7bf371b429c9c61f0c9556dbf9c609205db6474
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-03-08 00:24:28 +0600

    feat: :lipstick: created custom CheckboxgroupElement

--- vueform.config.ts
+++ vueform.config.ts
@@ -2,14 +2,17 @@ import en from '@vueform/vueform/locales/en'
 import vueform from '@vueform/vueform/dist/vueform'
 import { defineConfig } from '@vueform/vueform'
 
+import CheckboxgroupCheckbox_blocks from '~/components/ui/custom/CheckboxgroupCheckbox_blocks.vue'
+
 import '@vueform/vueform/dist/vueform.css';
 
 export default defineConfig({
+  classHelpers: true,
   displayErrors: false,
   floatPlaceholders: false,
   theme: vueform,
   locales: { en },
   locale: 'en',
-  classHelpers: true,
+  templates: { CheckboxgroupCheckbox_blocks },
   forceNumbers: true
 })
