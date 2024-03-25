ea23024b7eca36cb7d1310f43431df92e78d21ef
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-03-25 22:13:06 +0600

    feat: :construction: adds expansions to new games

--- components/game/new.client.vue
+++ components/game/new.client.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
 const id = crypto.randomUUID()
 
+const props = defineProps<{
+  expansions: ExpansionName[]
+}>()
+const { expansions } = toRefs(props)
+
 const emits = defineEmits<{
   submit: [game: IGameNew]
   cancel: []
 }>()
 
-function submit(form$: Vueform) {
+const filteredAncients = computed(() => {
+  return EXPANSIONS
+    .filter(expansion => expansions.value.includes(expansion.name))
+    .flatMap(expansion => expansion.ancinets)
+    .sort()
+})
+
+function onSubmit(form$: Vueform) {
   emits('submit', form$.requestData as IGameNew)
 }
 
-function cancel() {
+function onCancel() {
   emits('cancel')
 }
 </script>
 
 <template>
 <div :class="css.container">
-  <Vueform :class="css.form" :endpoint="false" @submit="submit">
+  <Vueform :class="css.form" :endpoint="false" @submit="onSubmit">
     <StaticElement name="head" tag="h2" content="Create a new game" />
     <!-- ID -->
     <HiddenElement name="id" :default="id" />
@@ -25,17 +37,17 @@ function cancel() {
       <!-- Date -->
       <DateElement name="date" label="Date" :default="new Date()" display-format="MMMM DD, YYYY" />
       <!-- AncientName -->
-      <SelectElement name="ancientName" label="Ancient One" :default="ANCIENT.AZATHOTH" :items="ANCIENT_NAMES" />
+      <SelectElement name="ancientName" label="Ancient One" :default="ANCIENT.AZATHOTH" :items="filteredAncients" />
       <!-- PlayerCount -->
       <SelectElement name="playerCount" label="Number of players" :default="4" :items="arr(8)" />
     </ObjectElement>
     <ObjectElement name="results">
       <!-- IsWin -->
-      <ToggleElement name="isWin" text="Defeat" :default="false" :labels="{ on: 'Win', off: 'Loss' }" />
+      <ToggleElement name="isWin" label="Defeat" :default="false" :labels="{ on: 'Win', off: 'Loss' }" />
     </ObjectElement>
     <!-- Submit buttons -->
     <GroupElement :class="css.buttons" name="buttons">
-      <ButtonElement name="cancel" button-label="Cancel" secondary @click="cancel" :columns="6" />
+      <ButtonElement name="cancel" button-label="Cancel" secondary @click="onCancel" :columns="6" />
       <ButtonElement name="submit" button-label="Create game" align="right" submits :columns="6" />
     </GroupElement>
   </Vueform>
@@ -46,7 +58,6 @@ function cancel() {
 .container {
   display: flex;
   justify-content: center;
-  margin: 20px;
 }
 
 .form {
