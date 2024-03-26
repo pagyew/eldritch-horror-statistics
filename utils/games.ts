093d38400e28d0a45495696c6e6f696f2512fe53
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-03-26 21:16:22 +0600

    feat: adds overloads to `calculateScore` utility

--- utils/games.ts
+++ utils/games.ts
@@ -6,8 +6,12 @@ export function isScored<T extends IGame>(game: T): game is T & IGameWinFilled {
   return typeof game.results.scores !== 'undefined'
 }
 
-export function calculateScore(game: IGameWinFilled) {
-  const { gates, monsters, cursed, rumors, clues, blessed, doom } = game.results.scores
+export function calculateScore(game: IGameWinFilled): number
+export function calculateScore(scores: IScores): number
+export function calculateScore(gameOrScores: IGameWinFilled | IScores) {
+  const scores = 'results' in gameOrScores ? gameOrScores.results.scores : gameOrScores
+  const { gates, monsters, cursed, rumors, clues, blessed, doom } = scores
+
   return (
     gates
     + Math.ceil(monsters / 3)
