94e4a735f89870b327c8ca2ab19e4104c7943105
Vladislav Tsepilov <fickleprogger@gmail.com>
2024-03-26 22:45:54 +0600

    feat: creates game's views

---
+++ views/game/general.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  date: string
+  ancientName: AncientName
+  playerCount: number
+}>()
+const { date, ancientName, playerCount } = toRefs(props)
+
+const emits = defineEmits<{
+  editClick: [type: 'general']
+}>()
+</script>
+
+<template>
+<ViewGameBlock type="general" @edit-click="emits('editClick', 'general')">
+  <ViewGameLine left="Ancient" :right="ancientName" />
+  <ViewGameLine left="Players" :right="playerCount" />
+  <ViewGameLine left="Date" :right="date" />
+</ViewGameBlock>
+</template>
+
+<style module="css"></style>
---
+++ views/game/rules.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+const props = withDefaults(defineProps<{
+  preludeName?: PreludeName
+  hasStartingRumor?: boolean
+  mythos?: IGameMythos[]
+}>(), {
+  preludeName: PRELUDE.NONE,
+  hasStartingRumor: false,
+  mythos: () => [...MYTH_TYPES]
+})
+const { preludeName, hasStartingRumor, mythos } = toRefs(props)
+
+const emits = defineEmits<{
+  editClick: [type: 'rules']
+}>()
+</script>
+
+<template>
+<ViewGameBlock type="rules" @edit-click="emits('editClick', 'rules')">
+  <ViewGameLine left="Mythos" :right="join(mythos)" />
+  <ViewGameLine left="Prelude" :right="preludeName" />
+  <ViewGameLine left="Rumor" :right="hasStartingRumor" />
+</ViewGameBlock>
+</template>
+
+<style module="css"></style>
